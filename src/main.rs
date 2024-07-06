7453716e638a5ddc5fc978c690271041d655ccbb
collinschacha <chachacollinsmugendi@gmail.com>
2024-07-06 21:04:45 +0300

    added rust imaging library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-    println!("HEllo world");
-    test();
-}
+use image::io::Reader as ImageReader;
 
-fn test() {
-    println!("This is a test");
-    println!("This is a test");
+use std::env::args;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = args().collect();
+    let img_path = &args[1];
+    let img = ImageReader::open(img_path)?.decode()?;
+    let img_grey = img.to_luma8();
+    dbg!(img_grey);
+
+    Ok(())
 }
